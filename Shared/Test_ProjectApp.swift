67020bdd97f8e5e16fe8b29a79877a1546542622
shulme801 <shulme801@gmail.com>
2022-05-08 12:34:13 -0400

    Initial Commit

---
+++ Shared/Test_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_ProjectApp.swift
+//  Shared
+//
+//  Created by Stephen Hulme on 5/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Test_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
